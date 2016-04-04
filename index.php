5d43de50daa6a1527c34f5d60a9847ef1a858623
Marco Comassetto <marcommas@gmail.com>
2016-04-04 11:45:08 -0300

    Adicionado footer com redes sociais

---
+++ index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" href="img/favicon.png" type="image/png" />
+	<title>Marco Comassetto</title>
+	
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	
+	<!--[if lt IE 9]>
+	<script src="js/modernizr.min.js" type="text/javascript" ></script>
+	<script src="js/html5shiv.min.js" type="text/javascript" ></script>
+	<![endif]-->
+</head>
+<body>
+	
+		
+	
+	<header class="header">
+		
+			<div class="nome">
+				<h2><span style="font-size: 40px;">M</span>arco <span style="font-size: 40px;">C</span>omassetto</h2>
+				<h5>Desenvolvedor de Software</h5>
+			</div>
+			
+			<nav>
+				<ul class="menu">
+					<li>
+						<a href=""  ><i class="fa fa-3x fa-home"></i> HOME</a>
+					</li>
+					<li>
+						<a href=""><i class="fa fa-3x fa-user"></i> SOBRE</a>
+					</li>
+					<li>
+						<a href=""><i class="fa fa-3x fa-desktop"></i> PORTFÓLIO</a>
+					</li>
+					<li>
+						<a href=""><i class="fa fa-3x fa-envelope-o"></i> CONTATO</a>
+					</li>
+				</ul>
+			</nav>
+
+		
+	</header>
+	<!-- Final do Header-->
+
+	<div class="clear"></div>
+
+	<section class="slide text-center backgound-parallax-slide color-white">
+
+			<h1 class="titulo-slide">Bem-vindo ao meu Portfólio</h1>
+			<p class="descricao-slide">Navegue pelo site e conheça um pouco mais sobre mim :)</p>
+
+	</section>	
+	<!--Final da Section>-->
+	
+	<section class="sobre padding-40">
+		<div class="container ">
+			<h1 class="titulo"><i class="fa fa-2x fa-home"></i> Quem sou eu?</h1>
+			<div class="divider"></div>
+			
+			<div class="col-md-8 col-sm-6 padding-80">
+				<blockquote>
+					<h4>Meu nome é <strong>Marco Antônio Comassetto</strong>, sou natural de Concórdia, Santa Catarina, atualmente moro em Florianópolis, tenho 24 anos e sou <strong>Bacharel em Sistemas de Informação</strong> pela Universidade do Estado de Santa Catarina.</h4>
+				</blockquote>
+				
+			</div>
+			
+			<div class="col-md-4 col-sm-6 padding-40">
+				<img src="img/marco.jpg" id="minhaFoto" class="img-circle img-responsive"  alt="Marco Comassetto">
+			</div >
+			
+			<div class="col-md-12">
+				<article class="col-md-6">
+					<h1 class="text-center">Experiência</h1>
+
+
+					<div class="col-sm-6">
+						<h4>Genialtec Soluções em Tecnologia</h4>
+						<h5>set/2015 - mar/2016</h5>
+					</div>
+
+					<div class="col-sm-6">
+						<h4>Programador</h4>
+						<h5>Experiência no planejamento, análise, modelagem e desenvolvimento de softwares, envolvendo a aplicação de linguagens e ambientes de programação</h5>
+						
+					</div>
+
+					<div class="col-sm-12"><div class="divider-dashed"></div></div>
+
+					<div class="col-sm-6">
+						<h4>Genialtec Soluções em Tecnologia</h4>
+						<h5>set/2013 - set/2015</h5>
+					</div>
+
+					<div class="col-sm-6">
+						<h4>Estágio de Programaçãoo</h4>
+						<h5>Atuação na implantação de códigos executáveis e testes unitários e integrados do seu funcionamento, possibilitando a identificação e correção de possíveis exceções e erros</h5>
+
+					</div>
+					
+					<div class="col-sm-12"><div class="divider-dashed"></div></div>	
+
+					<div class="col-sm-6">
+						<h4>Universidade do Estado de Santa Catarina</h4>
+						<h5>mar/2013 - jul/2014</h5>
+					</div>
+
+					<div class="col-sm-6">
+						<h4>Monitor de Programação</h4>
+						<h5>Responsável pelo auxílio aos alunos na disciplina de programação orientados a objetos na linguagem Java. </h5>
+					</div>
+
+				</article>
+
+				<article class="col-md-6" >
+					<h1 class="text-center">Tecnologia</h1>
+					
+					<div class="progress">
+						<div class="progress-bar progress-bar-success texto-progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 80%; text-align: left; font-size: 19px;">
+						PHP
+						</div>
+
+					</div>
+					
+					<div class="progress">
+						<div class="progress-bar progress-bar-success texto-progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 90%; text-align: left; font-size: 19px;">
+						HTML5 / CSS3
+						</div>
+
+					</div>
+
+					<div class="progress">
+						<div class="progress-bar progress-bar-success texto-progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 70%; text-align: left; 	font-size: 19px; ">
+						JavaScript
+						</div>
+
+					</div>
+
+					<div class="progress">
+						<div class="progress-bar progress-bar-success texto-progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 60%; text-align: left; font-size: 19px;">
+						Laravel
+						</div>
+
+					</div>
+
+					<div class="progress">
+						<div class="progress-bar progress-bar-success texto-progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 90%; text-align: left; font-size: 19px;">
+						Bootstrap
+						</div>
+
+					</div>
+					
+					
+					<div class="progress">
+						<div class="progress-bar progress-bar-success texto-progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 80%; text-align: left; font-size: 19px;">
+						C#
+						</div>
+
+					</div>
+
+					<div class="progress">
+						<div class="progress-bar progress-bar-success texto-progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 90%; text-align: left; font-size: 19px;">
+						MySQL
+						</div>
+
+					</div>
+
+					<div class="progress">
+						<div class="progress-bar progress-bar-success texto-progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 80%; text-align: left; font-size: 19px;">
+						SQL Server
+						</div>
+
+					</div>
+
+					<div class="progress">
+						<div class="progress-bar progress-bar-success texto-progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 70%; text-align: left; font-size: 19px;">
+						Firebird
+						</div>
+
+					</div>
+				</article>
+			</div>
+		</div>
+
+		
+	</section>
+	<!--Final da Sobre>-->
+
+
+	<section class="portfolio padding-40">
+		<div class="container">
+			<h1 class="titulo"><i class="fa fa-2x fa-desktop"></i> Portfólio</h1>
+			<div class="divider"></div>
+			
+			<div class="padding-40 "></div>
+
+			<div class="col-sm-6 col-md-4">
+				<h1 class="tituloImgPortfolio">Gerenciamento de Questionário</h1>
+				<a href="">
+					<img src="img/GTQuest.png" class="imgPortfolio" alt="GTQuest" >
+				</a>
+				<h6>Saiba mais</h6>
+			</div>
+
+			<div class="col-sm-6 col-md-4">
+				<h1 class="tituloImgPortfolio">Software para Academia</h1>
+				<a href="">
+					<img src="img/ActionBody.png" class="imgPortfolio"  alt="ActionBody">
+				</a>
+				<h6>Saiba mais</h6>
+			</div>
+
+			<div class="col-sm-6 col-md-4">
+				<h1 class="tituloImgPortfolio">Visualização de Vídeos</h1>
+				<a href="">
+					<img src="img/ActionBody.png" class="imgPortfolio"  alt="Alston">
+				</a>
+				<h6>Saiba mais</h6>
+			</div>
+			
+
+			<div class="col-sm-6 col-md-4">
+				<h1 class="tituloImgPortfolio">Gerenciamento de Questionário</h1>
+				<a href="">
+					<img src="img/GTQuest.png" class="imgPortfolio" alt="GTQuest" >
+				</a>
+				<h6>Saiba mais</h6>
+			</div>
+
+			<div class="col-sm-6 col-md-4">
+				<h1 class="tituloImgPortfolio">Software para Academia</h1>
+				<a href="">
+					<img src="img/ActionBody.png" class="imgPortfolio"  alt="ActionBody">
+				</a>
+				<h6>Saiba mais</h6>
+			</div>
+
+			<div class="col-sm-6 col-md-4">
+				<h1 class="tituloImgPortfolio">Visualização de Vídeos</h1>
+				<a href="">
+					<img src="img/ActionBody.png" class="imgPortfolio"  alt="Alston">
+				</a>
+				<h6>Saiba mais</h6>
+			</div>
+
+			
+
+		</div>
+	</section>
+	<!--Final da Portfolio>-->
+
+	<section class="contato padding-40">
+		<div class="container">
+			<h1 class="titulo"><i class="fa fa-2x fa-envelope-o"></i> Contato</h1>
+			<div class="divider"></div>
+			
+			<div class="padding-40 "></div>
+			
+
+
+		
+
+		<div class="class-form">
+			<form action="" class="formularioContato">
+				<input type="text" name="nome" placeholder="Nome">
+				<input type="email" name="email" placeholder="E-mail">
+				<input type="text" name="assunto" placeholder="Assunto">
+				<textarea name="mensagem" id="" cols="30" rows="10" placeholder="Mensagem"></textarea>
+
+				<input type="submit" name="enviar" >				
+			</form>
+		</div>
+
+		</div>
+
+	</section>
+	<!--Final sessão contato -->
+
+
+	<section class="copyright text-center ">
+
+		<a href="https://github.com/marcommas" target="_blank">
+			<img src="img/sociais/github.png" class="imgSociais" alt="Github: marcommas">
+		</a>
+		
+		<a href="https://br.linkedin.com/in/marco-antônio-comassetto-3a80507b" target="_blank">
+			<img src="img/sociais/linkedin.png" class="imgSociais" alt="Linkedin: Marco Comassetto">
+		</a>
+
+		<a href="https://www.facebook.com/marco.comassetto" target="_blank">
+			<img src="img/sociais/facebook.png" class="imgSociais" alt="Facebook: Marco Comassetto">
+		</a>	
+
+		
+		<img src="img/sociais/skype.png" class="imgSociais" alt="skype: marco.desenvolvimento">
+		
+
+		<a href="mailto:marcommas@gmail.com" >
+			<img src="img/sociais/gmail.png" class="imgSociais" alt="Gmail: marcommas@gmail.com">
+		</a>	
+
+		<p class="copyright padding-20">© Copyright <?php echo date('Y');?> - Desenvolvido por Marco Comassetto</p>
+	</section>
+
+
+	<script src="js/jquery-2.2.2.min.js" type="text/javascript" ></script>
+	<script src="js/bootstrap.min.js" type="text/javascript" ></script>
+</body>
+</html>
