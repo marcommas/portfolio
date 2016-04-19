d9d4f6b48e98577a2790df67366da5aeca993716
Marco Comassetto <marcommas@gmail.com>
2016-04-19 20:12:27 -0300

    realizada tela de cronicas

---
+++ cronica.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" href="img/favicon.png" type="image/png" />
+	<title>Impressão de Crônicas</title>
+	
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/lightbox/lightbox.min.css">
+
+	
+
+	<!--[if lt IE 9]>
+	<script src="js/modernizr.min.js" type="text/javascript" ></script>
+	<script src="js/html5shiv.min.js" type="text/javascript" ></script>
+	<![endif]-->
+
+</head>
+<body>
+	
+
+	<nav class="navbar header navbar-fixed-top">
+
+		<div class="navbar-header">
+		    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			    <span class="sr-only">Toggle navigation</span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+		    </button>
+
+		    
+	    	<a href="index.php" class="nome" >
+	    		<img src="img/logo.png" class="img-responsive" style="max-width: 240px;">
+	    	</a>
+			
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+		    <ul class="nav menu navbar-nav navbar-right cl-effect-3 ">
+
+				<li>
+					<a href="index.php#sobre"> Sobre</a>
+				</li>
+				<li>
+					<a href="index.php#portfolio"> Portfólio</a>
+				</li>
+				<li>
+					<a href="index.php#contato"> Contato</a>
+				</li>
+		    </ul>
+		</div>
+
+	</nav>
+
+	<!-- Final do Header-->
+
+	<div class="clear"></div>
+
+	
+
+
+	<section id="portfolio" class="portfolio padding-100">
+		<div class="container">
+			<h1 class="titulo"> Software para Impressão de Crônicas</h1>
+			<div class="divider"></div>
+			
+			<div class="row">
+				<div class="padding-40 col-md-6">
+					<a href="img/cronica/cronica1.png" data-lightbox="example-set" data-title="Tela para a impressão da crônica.">
+						<img class="imagemPrincipal img-responsive" src="img/cronica/cronica1.png" alt=""/>
+					</a>
+
+					<div class="padding-10">
+						<a href="img/cronica/cronica2.png" data-lightbox="example-set" data-title="Home do painel administrativo.">
+							<img class="imagemSecundaria col-md-4 img-responsive" src="img/cronica/cronica2.png" alt=""/>
+						</a>
+
+						<a href="img/cronica/cronica3.png" data-lightbox="example-set" data-title="Lista de usuários cadastrados.">
+							<img class="imagemSecundaria col-md-4 img-responsive" src="img/cronica/cronica3.png" alt=""/>
+						</a>
+
+						<a href="img/cronica/cronica4.png" data-lightbox="example-set" data-title="Tela para cadastro ou alteração de usuários.">
+							<img class="imagemSecundaria col-md-4 img-responsive" src="img/cronica/cronica4.png" alt=""/>
+						</a>
+						
+						<!-- Imagens escodidas, somente para a galeria  -->
+						<a href="img/cronica/cronica5.png" data-lightbox="example-set" data-title="Tela para cadastro ou alteração de crônicas." style="display: none;"></a>
+
+						<a href="img/cronica/cronica6.png" data-lightbox="example-set" data-title="Relatório com determinados filtros." style="display: none;"></a>
+
+						<a href="img/cronica/cronica7.png" data-lightbox="example-set" data-title="Relatório com resultado." style="display: none;"></a>
+
+						
+					</div>
+				</div>
+
+				<div class="padding-40 col-md-6">
+
+					<h2 class="text-center">Descrição</h2>
+					<h4><i class="fa fa-check"></i> Ferramenta web para impressão de crônicas em terminais de autoatendimeto.</h4>
+					<h4><i class="fa fa-check"></i> Desenvolvimento utilizando o padrão mvc.</h4>
+					<h4><i class="fa fa-check"></i> Painel administrativo para gerencimento do software.</h4>
+					<h4><i class="fa fa-check"></i> Relatório de quantidade de impressões realizado por período.</h4>
+
+					
+
+
+
+					<h2 class="text-center padding-20">Tecnologias Utilizadas</h2>
+
+					<ul class="tecnologias text-center ">
+						<li><img class="imgTecnologia img-responsive" src="img/tecnologias/html5.png" alt="HTML5" title="HTML5"/></li>
+						<li><img class="imgTecnologia img-responsive" src="img/tecnologias/css3.png" alt="CSS3" title="CSS3"/></li>
+						<li><img class="imgTecnologia img-responsive" src="img/tecnologias/js.png" alt="JavaScript" title="JavaScript"/></li>
+						<li><img class="imgTecnologia img-responsive" src="img/tecnologias/php.png" alt="PHP" title="PHP"/></li>
+						<li><img class="imgTecnologia img-responsive" src="img/tecnologias/laravel.png" alt="Laravel" title="Laravel"/></li>
+						<li><img class="imgTecnologia img-responsive" src="img/tecnologias/bootstrap.png" alt="Twitter Bootstrap" title="Twitter Bootstrap"/></li>
+						<li><img class="imgTecnologia img-responsive" src="img/tecnologias/mysql.png" alt="MySQL" title="MySQL"/></li>
+
+						<li><img class="imgTecnologia img-responsive" src="img/tecnologias/responsivo.png" alt="Responsivo" title="Responsivo"/></li>
+						<li><img class="imgTecnologia img-responsive" src="img/tecnologias/amazon.png" alt="Amazon" title="Amazon"/></li>
+
+			
+
+					
+					
+						
+					</ul>	
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+
+
+
+
+	<footer id="footer">
+		<div class="text-center">
+			<a href="https://github.com/marcommas" target="_blank">
+				<img src="img/sociais/github.png" class="imgSociais" alt="Github: marcommas" title="Github: marcommas">
+			</a>
+			
+			<a href="https://br.linkedin.com/in/marco-antônio-comassetto-3a80507b" target="_blank">
+				<img src="img/sociais/linkedin.png" class="imgSociais" alt="Linkedin: Marco Comassetto" title="Linkedin: Marco Comassetto">
+			</a>
+
+			<a href="https://www.facebook.com/marco.comassetto" target="_blank">
+				<img src="img/sociais/facebook.png" class="imgSociais" alt="Facebook: Marco Comassetto" title="Facebook: Marco Comassetto">
+			</a>	
+
+			
+			<img src="img/sociais/skype.png" class="imgSociais" alt="skype: marco.desenvolvimento" title="skype: marco.desenvolvimento">
+			
+
+			<a href="mailto:marcommas@gmail.com" >
+				<img src="img/sociais/gmail.png" class="imgSociais" alt="Gmail: marcommas@gmail.com" title="Gmail: marcommas@gmail.com">
+			</a>		
+
+			<p class="copyright padding-10">© Copyright <?php echo date('Y');?> - Desenvolvido por Marco Comassetto</p>
+
+		</div>
+		
+	</footer>
+
+	<script src="js/jquery-2.2.2.min.js" type="text/javascript" ></script>
+	<script src="js/bootstrap.min.js" type="text/javascript" ></script>
+
+	<script src="js/lightbox/lightbox-plus-jquery.min.js"></script>
+	
+
+</body>
+</html>
